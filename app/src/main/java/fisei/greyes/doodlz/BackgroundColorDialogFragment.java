7d5a124fd486029b9dffda47b418621a0c2cc388
Pablo Alvear <alejozx456@gmail.com>
2022-05-26 18:28:29 -0500

    first commit

---
+++ app/src/main/java/fisei/greyes/doodlz/BackgroundColorDialogFragment.java
@@ -0,0 +1,20 @@
+package fisei.greyes.doodlz;
+
+import com.deitel.doodlz.R;
+
+public class BackgroundColorDialogFragment extends ColorDialogFragment {
+    @Override
+    protected int getTitle() {
+        return R.string.title_background_color_dialog;
+    }
+
+    @Override
+    protected int getColorDoodleView(DoodleView doodleView){
+        return doodleView.getDrawingBackgroundColor();
+    }
+
+    @Override
+    protected void setColorDoodleView(DoodleView doodleView) {
+        doodleView.setDrawingBackgroundColor(color);
+    }
+}
